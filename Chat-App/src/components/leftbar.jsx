0bf39142c21e3d7e07af36294e8beeadb31338ba
Faizal1010 <buckyte6664@gmail.com>
2024-03-29 15:52:55 +0530

    leftbar

--- Chat-App/src/components/leftbar.jsx
+++ Chat-App/src/components/leftbar.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
-import viteLogo from '/vite.svg'
-import reactLogo from '../assets/react.svg'
-import '../components/leftbar.css'
+import React from "react";
+import viteLogo from "/vite.svg";
+import reactLogo from "../assets/react.svg";
+import "../components/leftbar.css";
 
 const leftbar = () => {
   return (
     <div>
       <div className="left">
-    <div className="logo">
-      <img src={viteLogo}/>
-      <img src={reactLogo}/>
-    </div>
-    </div>
+        <div className="logo">
+          <img src={viteLogo} />
+          <img src={reactLogo} />
+        </div>
 
-    </div>
-  )
-}
+        <div className="search">
+          <input placeholder="Search user or chat" id="search">
+          </input>
+        </div>
 
-export default leftbar
+      </div>
+    </div>
+  );
+};
+
+export default leftbar;
